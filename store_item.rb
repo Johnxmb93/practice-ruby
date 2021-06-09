850b393f232fb29cf5f3a9559a313d73a4165e2c
johnxmb93 <jonathan.mb93@gmail.com>
2021-06-09 13:43:40 -0500

    items list created

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+item1 = { name: "cookie", color: "brown", price: "$1" }
+item2 = { name: "soda", color: "red", price: "$2" }
+item3 = { name: "chips", color: "yellow", price: "$5" }
+p item1[:name]
+p item2[:color]
+p item3[:name]
+items = { name: ["gum", "candy", "iphone"], color: ["pink", "rainbow", "silver"], price: ["$3", "$4", "$700"] }
+p items
